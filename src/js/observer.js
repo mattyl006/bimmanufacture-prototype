9c79807ae6c34d4d73a8ee62a0c587684cafb698
Mateusz <wirus006@gmail.com>
2020-11-05 09:29:41 +0100

    initialize skelethon of sections scanning, bim and modeling

--- src/js/observer.js
+++ src/js/observer.js
@@ -4,12 +4,11 @@ const hero = document.querySelector('.hero--js');
 const navigation = document.querySelector('.navigation--js')
 
 const options = {
-    rootMargin: '-120px',
+    rootMargin: '-200px',
 };
 
 const observer = new IntersectionObserver(function (entries, observer) {
     entries.forEach(entry => {
-        console.log(entry);
         if(entry.isIntersecting) {
             navigation.classList.add('hide');
         }
